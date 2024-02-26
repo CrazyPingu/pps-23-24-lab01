9cc966ad3969f206db10d600a4056542e6aba8e8
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2024-02-26 11:47:16 +0100

    Add tests for CircularList

--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -1,17 +1,69 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import tdd.CircularList;
+import tdd.CircularListImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach() {
+        circularList = new CircularListImpl();
+    }
+
+    @Test
+    public void testAdd() {
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(3, circularList.size());
+    }
+
+    @Test
+    public void testNext() {
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(1, circularList.next().get());
+        assertEquals(2, circularList.next().get());
+        assertEquals(3, circularList.next().get());
+        assertEquals(1, circularList.next().get());
+    }
+
+    @Test
+    public void testPrevious() {
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(3, circularList.previous().get());
+        assertEquals(2, circularList.previous().get());
+        assertEquals(1, circularList.previous().get());
+        assertEquals(3, circularList.previous().get());
+    }
+
+    @Test
+    public void testReset() {
+        circularList.add(1);
+        circularList.add(2);
+        circularList.add(3);
+        assertEquals(1, circularList.next().get());
+        assertEquals(2, circularList.next().get());
+        assertEquals(3, circularList.next().get());
+        circularList.reset();
+        assertEquals(1, circularList.next().get());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(circularList.isEmpty());
+        circularList.add(1);
+        assertFalse(circularList.isEmpty());
     }
 
 }
